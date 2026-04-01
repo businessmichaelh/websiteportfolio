580bc0b712c4d2e864744ef2f338df8b7ec98466
businessmichaelh <business.michaelh@gmail.com>
2026-04-01 18:35:29 -0500

    Updated description, Updated buttons, allowed for multiple photos with arrows to swap between.

--- scripts.js
+++ scripts.js
@@ -10,3 +10,20 @@ closeBtn.addEventListener('click', () => {
   modal.classList.remove("open");
 });
 
+
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function moveSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    const slides = document.querySelectorAll(".slide");
+
+    if (n >= slides.length) slideIndex = 0;
+    if (n < 0) slideIndex = slides.length - 1;
+
+    slides.forEach(slide => slide.style.display = "none");
+    slides[slideIndex].style.display = "block";
+}
